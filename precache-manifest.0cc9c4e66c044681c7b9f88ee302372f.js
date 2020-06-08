51ed1fddc0258b2fcc5bfe80f942221b4894bf0f
Flavius <aflaviusandrei@gmail.com>
2020-06-08 22:41:27 +0300

    Updates

---
+++ precache-manifest.0cc9c4e66c044681c7b9f88ee302372f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f713e375f3461adccfc9b08cc0cdd8c5",
+    "url": "/aflandrei/index.html"
+  },
+  {
+    "revision": "3c6fdb4ba8a38ae72709",
+    "url": "/aflandrei/static/css/main.d9841ad6.chunk.css"
+  },
+  {
+    "revision": "c4761812f62a6d5b9c27",
+    "url": "/aflandrei/static/js/2.1ffed3fb.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/aflandrei/static/js/2.1ffed3fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6fdb4ba8a38ae72709",
+    "url": "/aflandrei/static/js/main.9f57998d.chunk.js"
+  },
+  {
+    "revision": "5ee1e7893ed171927919",
+    "url": "/aflandrei/static/js/runtime-main.60a31f64.js"
+  },
+  {
+    "revision": "f194fdcf0aacf0409b919590ffdeb060",
+    "url": "/aflandrei/static/media/Screenshot_1.f194fdcf.jpg"
+  },
+  {
+    "revision": "304282eae8c99d981de8ef460cb337be",
+    "url": "/aflandrei/static/media/Screenshot_2.304282ea.jpg"
+  },
+  {
+    "revision": "b5fb1955cad58d16ae7ed3e02d30091b",
+    "url": "/aflandrei/static/media/bc.b5fb1955.gif"
+  },
+  {
+    "revision": "583cf31c0c5e9566cd7d533f0f1ae0ae",
+    "url": "/aflandrei/static/media/condo.583cf31c.gif"
+  },
+  {
+    "revision": "95dd3d6cc346ff55ee22234651d2fc59",
+    "url": "/aflandrei/static/media/cv.95dd3d6c.pdf"
+  },
+  {
+    "revision": "97fe96b6682b796801bf9fca1e94e574",
+    "url": "/aflandrei/static/media/edociif.97fe96b6.gif"
+  },
+  {
+    "revision": "3704b73a770c8b730aaa29b3e7a8c523",
+    "url": "/aflandrei/static/media/fiicode.3704b73a.gif"
+  },
+  {
+    "revision": "8dfb8558d7d2658614be6265b0e9d1b2",
+    "url": "/aflandrei/static/media/gitnephew.8dfb8558.png"
+  },
+  {
+    "revision": "8eaf6d0f23b9d5bd3c2b7bff50b65760",
+    "url": "/aflandrei/static/media/gobbler.8eaf6d0f.gif"
+  },
+  {
+    "revision": "c24dc5ca723645cb0821e069351fefd9",
+    "url": "/aflandrei/static/media/typer.c24dc5ca.gif"
+  }
+]);
